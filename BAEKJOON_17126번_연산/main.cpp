f030fc21e5e46f54ba66e02293e218ea2df3fa75
이준형 <lhyoup@gmail.com>
2020-05-21 00:34:53 +0900

    문제 설명 및 실패한 풀이 추가

--- BAEKJOON_17126번_연산/main.cpp
+++ BAEKJOON_17126번_연산/main.cpp
@@ -1,36 +1,154 @@
 #include <stdio.h>
 #include <algorithm>
-#include <cmath>
+#include <map>
+#include <string.h>
+#define MAX_LENGTH 101
+
 using namespace std;
+typedef long long ll;
+
+map<string, int> m;
+	
+int getLength(string str)
+{
+	int idx = 0;
+	while(str[idx] != '\0')
+	{
+		idx++;
+	}
+	
+	return idx;
+}
+
+int stringCmp(string str1, string str2)
+{
+	int res = 0;
+	for(int i = 0; i < str1.length(); i++)
+	{
+		if(str1[i] > str2[i])
+		{
+			res = 1;
+			break;
+		}else if(str1[i] < str2[i])
+		{
+			res = -1;
+			break;
+		}
+	}
+	
+	return res;
+}
+
+ll sectionSum()
+{
+	ll section_sum = 0;
+	
+	char key1[MAX_LENGTH], key2[MAX_LENGTH];
+	scanf("%s %s", &key1, &key2);
+
+	for(auto it = m.begin(); it != m.end(); it++)
+	{
+		string it_key = it->first;
+		int it_key_len = it_key.length();
+		int key1_len = getLength(key1);
+		int key2_len = getLength(key2);
+		
+		//it_key string length lower or bigger
+		if(it_key_len < key1_len || key2_len < it_key_len) 
+		{
+			continue;
+		}
+		else if(key1_len < it_key_len && it_key_len < key2_len)
+		{
+			section_sum += it->second;	
+		}
+		else 
+		{
+			if((strcmp(it_key.c_str(), key1) == 0) || (strcmp(it_key.c_str(), key2) == 0)) 	section_sum += it->second;	
+			else if(key1_len == key2_len)
+			{
+				if((stringCmp(key1, it_key) == -1) && (stringCmp(key2, it_key) == 1))
+				{
+					section_sum += it->second;	
+				}
+			}
+			else if(key1_len == it_key_len)
+			{
+				if(stringCmp(key1, it_key) == -1)
+				{
+					section_sum += it->second;	
+					break;
+				}
+			}
+			else
+			{
+				if(stringCmp(key2, it_key) == 1)
+				{
+					section_sum += it->second;	
+					break;
+				}
+			}
+		}
+	}
+	
+	printf("%lld ", section_sum);
+}
 
 int main(void)
 {
-	double X, Y, C;
-	double left, right, w;
-	double h_x, h_y, h_mid;
+	int n;
+	int cmd, value;
+	char key[MAX_LENGTH];
 	
-	scanf("%lf %lf %lf", &X, &Y, &C);
-	left = 0;	
-	right = min(X,Y);
-
-	while(right - left > 0.000001)
+	ll sum = 0;
+	
+	scanf("%d", &n);
+	
+	for(int t = 0; t < n; t++)
 	{
-		w = (left + right) / 2;
-		
-		h_x = sqrt(X*X - w*w);
-		h_y = sqrt(Y*Y - w*w);
-		h_mid = (h_x*h_y) / (h_x+h_y);
-		
-		if(h_mid > C)
+		scanf("%d", &cmd);
+		switch(cmd)
 		{
-			left = w;	
-		}
-		else
-		{
-			right = w;
+			case 1 :
+			{
+				scanf("%s %d", &key, &value);
+				
+				auto add_it = m.find(key);
+				if(add_it != m.end()) 
+				{
+					add_it->second += value;
+				}
+				else
+				{
+					m.insert(make_pair(key, value));
+				}
+					
+				sum += value;
+				
+				printf("%lld ", sum);
+				break;
+			}
+			case 2 :
+			{
+				sectionSum();
+				break;
+			}
+			case 3 :
+			{
+				scanf("%s", &key);				
+				
+				auto sub_it = m.find(key);
+				if(sub_it != m.end()){
+					sum -= sub_it->second;
+					m.erase(sub_it);
+				}
+							
+				printf("%lld ", sum);
+				break;
+			}
 		}
 	}
-	printf("%.3lf\n", left);
+
 	
 	return 0;
 }
